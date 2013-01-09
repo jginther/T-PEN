299d4cfab425376e48c15b0055f6a60672b21cd7
Thom Hastings <tghastings@gmail.com>
2013-01-08 23:27:03 -0600

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
+echo "Updating..."
+echo
 cd mosesdecoder
+echo "Moses-SMT:"
 git pull
 cd ..
 cd tesseract-ocr
+echo "Tesseract-OCR:"
 git svn rebase
 cd ..
 cd TesseractTrainer
+echo "TesseractTrainer:"
 git pull
 cd ..
+echo
 echo "DONE"
