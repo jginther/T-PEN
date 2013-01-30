656ea1575c72536141f1cfe1cc1643ebe6fec5cd
thomhastings <tghastings@gmail.com>
2013-01-29 19:16:44 -0600

    port to submodules

--- mosesdecoder/util/have.hh
+++
@@ -1,9 +0,0 @@
-/* Optional packages.  You might want to integrate this with your build system e.g. config.h from ./configure. */
-#ifndef UTIL_HAVE__
-#define UTIL_HAVE__
-
-#ifndef HAVE_ICU
-//#define HAVE_ICU
-#endif
-
-#endif // UTIL_HAVE__
