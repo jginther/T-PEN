c0d540dd743b4cc818691e3771c4d3925b80309e
ksoona <attackvectorlinux@gmail.com>
2013-05-13 00:15:55 -0500

    irish template

--- update.sh
+++ update.sh
@@ -2,9 +2,9 @@
 echo "Updating..."
 echo
 echo "Moses-SMT:"
-if [ ! -e mosesdecoder ]
+if [ ! -e moses-smt ]
 then
-    git submodule add git://github.com/moses-smt/mosesdecoder.git
+    git submodule add git://github.com/moses-smt/mosesdecoder.git moses-smt
 else
     cd mosesdecoder
     git pull git://github.com/moses-smt/mosesdecoder.git master
@@ -20,12 +20,21 @@ else
     cd ..
 fi
 echo "TesseractTrainer:"
-if [ ! -e TesseractTrainer ]
+if [ ! -e tesseract-trainer ]
 then
-    git submodule add git://github.com/BaltoRouberol/TesseractTrainer.git
+    git submodule add git://github.com/BaltoRouberol/TesseractTrainer.git tesseract-trainer
 else
-    cd TesseractTrainer
+    cd tesseract-trainer
     git pull git://github.com/BaltoRouberol/TesseractTrainer.git master
+    cd ..
+fi
+echo "Tesseract-GLE-Unical:"
+if [ ! -e tesseract-gle-unical ]
+then
+    git svn clone http://tesseract-gle-uncial.googlecode.com/svn/trunk/ tesseract-gle-uncial
+else
+    cd tesseract-gle-unical
+    git svn rebase
     cd ..
 fi
 echo
