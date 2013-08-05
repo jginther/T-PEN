ce2c2f6dc849456e4278eac01a97dc6518b9edb3
Kenneth Soona <attackvectorlinux@gmail.com>
2013-08-05 14:49:26 -0500

    ran update.sh

--- update.sh
+++ update.sh
@@ -78,7 +78,7 @@ echo "~{¿} Do you want to git push?"
 echo "This saves the updated code (to github.com)"
 echo " [y/N]: "
 read $INPUT
-if [ $INPUT = ["Y"or"y"] ]
+if [ $INPUT = "Y" ] or[ $INPUT = "y" ]
 then
     git push origin master -u
 fi
