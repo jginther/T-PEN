e3acbdeafed32f76debd6337c9f5bf716c6ab578
Kenneth Soona <attackvectorlinux@gmail.com>
2013-08-05 14:18:47 -0500

    ran update.sh

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ This fork is by [Thom Hastings](http://turing.slu.edu/~hastint/) to integrate
 via [git svn](http://git-scm.com/docs/git-svn) and
 + [Moses-SMT](http://www.statmt.org/moses/) for _Statistical Machine Translation_  
 via [github](https://github.com/moses-smt/mosesdecoder) into the existing [T-PEN](http://t-pen.org) framework.
-+ Automatic training of Tesseract is done as [such](http://isbullsh.it/2012/06/Automatic-tesseract-training/) with [TesseractTrainer](https://github.com/BaltoRouberol/TesseractTrainer).  
++ Tesseract is trained [automatically](http://isbullsh.it/2012/06/Automatic-tesseract-training/) with [TesseractTrainer](https://github.com/BaltoRouberol/TesseractTrainer).  
 + A sample Tesseract data set is provided by Kevin Scannell, [Tesseract-GLE-Unical](https://code.google.com/p/tesseract-gle-unical/)
 
 To-Do
--- update.sh
+++ update.sh
@@ -7,52 +7,76 @@
 # Information Technology Services
 #
 #!/bin/sh
-echo "Updating..."
 echo
-echo "Statistical Machine Translation… Moses-SMT:"
+echo "-=+=-=+=-=+=-=+=-=+=-=+=-=+=-=+=-=+=-=+=-=+=-"
+echo " _____     ____  _____ _   _                  "
+echo "|_   _|   |  _ \\| ____| \\ | |  ___  _ __ __ _ "
+echo "  | |_____| |_) |  _| |  \\| | / _ \\| '__/ _\` |"
+echo "  | |_____|  __/| |___| |\\  || (_) | | | (_| |"
+echo "  |_|     |_|   |_____|_| \\_(_)___/|_|  \\__, |"
+echo "                                        |___/ "
+echo "-=+=-=+=-=+=-=+=-=+=-=+=-=+=-=+=-=+=-=+=-=+=-"
+echo "Welcome to the T-PEN.org backend Update Script"
+echo "-=+=-=+=-=+=-=+=-=+=-=+=-=+=-=+=-=+=-=+=-=+=-"
+echo
+echo "_(¡) Updating Components!"
+echo 
+echo "+(1) Statistical Machine Translation ...… Moses-SMT:…"
 if [ ! -e moses-smt ]
 then
-	echo "adding submodule"
+	echo ")adding submodule"
     git submodule add git://github.com/moses-smt/mosesdecoder.git moses-smt
 else
-	echo "updating submodule"
+	echo ")updating submodule"
     cd moses-smt
     git pull git://github.com/moses-smt/mosesdecoder.git master
     cd ..
 fi
-echo "Optical Character Recognition… Tesseract-OCR:"
+echo
+echo "+(2) Optical Character Recognition ...… Tesseract-OCR:…"
 if [ ! -e tesseract-ocr ]
 then
-	echo "cloning via git-svn"
+	echo ")cloning via git-svn"
     git svn clone http://tesseract-ocr.googlecode.com/svn tesseract-ocr
 else
-	echo "git-svn rebase"
+	echo ")git-svn rebase"
     cd tesseract-ocr
     git svn rebase
     cd ..
 fi
-echo "Toolset for training Tesseract data… TesseractTrainer:"
+echo
+echo "=(3) Toolset for training Tesseract data ...… TesseractTrainer:…"
 if [ ! -e tesseract-trainer ]
 then
-	echo "adding submodule"
+	echo ")adding submodule"
     git submodule add git://github.com/BaltoRouberol/TesseractTrainer.git tesseract-trainer
 else
-	echo "updating submodule"
+	echo ")updating submodule"
     cd tesseract-trainer
     git pull git://github.com/BaltoRouberol/TesseractTrainer.git master
     cd ..
 fi
-echo "Example data (courtesy of Kevin Scannell)… Tesseract-GLE-Unical:"
+echo
+echo "-(4) Example data (courtesy of Kevin Scannell) ...… Tesseract-GLE-Unical:…"
 if [ ! -e tesseract-gle-unical ]
 then
-	echo "cloning via git-svn"
+	echo ")cloning via git-svn"
     git svn clone http://tesseract-gle-uncial.googlecode.com/svn/trunk/ tesseract-gle-uncial
 else
-	echo "git-svn rebase"
+	echo ")git-svn rebase"
     cd tesseract-gle-unical
     git svn rebase
     cd ..
 fi
 echo
-echo "DONE"
-
+git commit -a -m "ran update.sh"
+echo "_(¡) DONE with update script..."
+echo
+echo "~{¿} Do you want to \'git push\'?"
+echo "This saves the updated code (to github.com)"
+echo " [y/N]: "
+if [ $INPUT == "Y" ] or [ $INPUT == "y" ]
+then
+    git push origin master -u
+echo
+echo "_(¡) Goodbye!"
